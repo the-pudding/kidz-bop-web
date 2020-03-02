1ae5770261621db35ce1daba2e8ec2c9614ec57f
Jan Diehm <jadiehm@gmail.com>
2020-03-02 15:00:55 -0600

    tapping start

--- src/js/graphic.js
+++ src/js/graphic.js
@@ -1,8 +1,21 @@
 /* global d3 */
 function resize() {}
 
+const $fwdTapDiv = d3.selectAll('#right')
+const $bckTapDiv = d3.selectAll('#left')
+
+function fwdTap() {
+  console.log('fwd')
+}
+
+function bckTap() {
+  console.log('bck')
+}
+
 function init() {
-  console.log('Make something awesome!');
+  console.log('checking')
+  $fwdTapDiv.on('click', fwdTap)
+  $bckTapDiv.on('click', bckTap)
 }
 
 export default { init, resize };
